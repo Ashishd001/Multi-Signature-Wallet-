801db0be64d9f74248cb998b30ceca10fcbebac2
Ashishd001 <ashishdwivedi6780@gmail.com>
2025-06-20 13:59:10 +0530

    Update Multi-Signature Wallet.sol

--- Multi-Signature Wallet.sol
+++ Multi-Signature Wallet.sol
@@ -189,9 +189,11 @@ contract MultiSignatureWallet {
     }
 
 
-project:0xc72f65eB84f635b582fBF38DEC51b158F9F984A3
+
 
     function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
 }
+
+project:0xc72f65eB84f635b582fBF38DEC51b158F9F984A3
